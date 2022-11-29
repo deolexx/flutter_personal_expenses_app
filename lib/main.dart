8ec1502fbcc9d0ee0c7e7dc77c76bb7ddc2384a2
s.lukianenko <lukianenko88@gmail.com>
2022-11-29 21:00:25 +0200

    add image to the app and spacing with sized box

--- lib/main.dart
+++ lib/main.dart
@@ -51,10 +51,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-        id: 't1', title: 'Morning cofee', amount: 45.0, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'New Shoes', amount: 155.50, date: DateTime.now()),
+    // Transaction(
+    //     id: 't1', title: 'Morning cofee', amount: 45.0, date: DateTime.now()),
+    // Transaction(
+    //     id: 't2', title: 'New Shoes', amount: 155.50, date: DateTime.now()),
   ];
 
   void _addNewTransaction(String title, double amount) {
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,7 +4,7 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  TransactionList( this.transactions);
+  TransactionList(this.transactions);
 
   final List<Transaction> transactions;
 
@@ -12,46 +12,66 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 450,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-            child: Row(children: [
-              Container(
-                  padding:  EdgeInsets.all(10),
-                  margin:  EdgeInsets.symmetric(
-                      vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Theme.of(context).colorScheme.primary, width: 5),
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).appBarTheme.toolbarTextStyle,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
-                  child: Text(
-                    '\$${transactions[index].amount.toStringAsFixed(2)}',
-                    style:  TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).colorScheme.primary),
-                  )),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    transactions[index].title,
-                    style:  Theme.of(context).appBarTheme.toolbarTextStyle,
-                  ),
-                  Text(
-                    DateFormat.yMMMd().format(transactions[index].date),
-                    style:  TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey,
-                    ),
-                  )
-                ],
-              )
-            ]),
-          );
-          },
-        itemCount: transactions.length,
-
-      ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Row(children: [
+                    Container(
+                        padding: EdgeInsets.all(10),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Theme.of(context).colorScheme.primary,
+                              width: 5),
+                        ),
+                        child: Text(
+                          '\$${transactions[index].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).colorScheme.primary),
+                        )),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          transactions[index].title,
+                          style: Theme.of(context).appBarTheme.toolbarTextStyle,
+                        ),
+                        Text(
+                          DateFormat.yMMMd().format(transactions[index].date),
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    )
+                  ]),
+                );
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
