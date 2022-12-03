26b912eada21255208b3d2656ac570e77a283f6b
s.lukianenko <lukianenko88@gmail.com>
2022-12-03 19:07:58 +0200

    create ChartBar widget

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  const ChartBar(
+      {Key? key,
+      required this.label,
+      required this.spendingAmount,
+      required this.spendingPctOfTotal})
+      : super(key: key);
+
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('\$${spendingAmount.toStringAsFixed((0))}'),
+        SizedBox(
+          height: 5,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(children: [
+            Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.grey,
+                    ))),
+            FractionallySizedBox(
+              heightFactor: spendingPctOfTotal,
+              child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Theme.of(context).primaryColor)),
+            )
+          ]),
+        )
+      ],
+    );
+  }
+}
