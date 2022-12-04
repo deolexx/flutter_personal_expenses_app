9f999164f3d2dac990de165252b4e342f2df2272
s.lukianenko <lukianenko88@gmail.com>
2022-12-04 09:42:37 +0200

    add scaling constraints with LayoutBuilder

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -15,35 +15,51 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        FittedBox(child: Text('\$${NumberFormat.compact().format(spendingAmount.round())}')),
-        SizedBox(
-          height: 5,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(children: [
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return Column(
+          children: [
             Container(
-                decoration: BoxDecoration(
-                    color: Colors.grey.shade300,
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                      width: 1,
-                      color: Colors.grey,
-                    ))),
-            FractionallySizedBox(
-              heightFactor: spendingPctOfTotal,
-              child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Theme.of(context).colorScheme.primary)),
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(
+                    '\$${NumberFormat.compact().format(spendingAmount.round())}'),
+              ),
+            ),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+              height: constraints.maxHeight * 0.6,
+              width: 10,
+              child: Stack(children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey,
+                        ))),
+                FractionallySizedBox(
+                  heightFactor: spendingPctOfTotal,
+                  child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Theme.of(context).colorScheme.primary)),
+                )
+              ]),
+            ),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(child: Text(label.substring(0, 1))),
             )
-          ]),
-        ),
-        Text(label.substring(0, 1))
-      ],
+          ],
+        );
+      },
     );
   }
 }
